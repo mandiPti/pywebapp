7e5196f107d7492768f8207032c68518a636f4d1
mapi22 <jakkluu.to@gmail.com>
2022-01-27 09:47:41 +0100

    changed webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,12 @@
-a
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def default():
+    html = "<h3>Hello World from Flask App!</h3><h1>BUH!</h1>" 
+    return html
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
