a83b8fc671ecad556a7509cbb561f81dd54ca5c0
mapi22 <jakkluu.to@gmail.com>
2022-01-27 16:23:50 +0100

    nach Einrichten von github-actions

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def default():
-    html = "<h3>Hello World from Flask App!</h3><h1>BUH!</h1>" 
+    html = "<h3>Hello World from Flask App!</h3><h1>HUI-BUI!</h1>" 
     return html
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-
